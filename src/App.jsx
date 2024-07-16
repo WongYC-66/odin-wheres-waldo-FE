50a1d0c0fbf23b704560665c57cfd457f8a0cc97
WongYC-66 <yengchien@hotmail.com>
2024-07-16 12:07:30 +0800

    add Game01 img

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
   const [showPage, setShowPage] = useState("home")
 
   return (
-    <div className='d-flex flex-column vh-100 p-2 bg-danger-subtle'>
-      <main className='flex-fill m-2'>
+    <div className='d-flex flex-column min-vh-100 overflow-x-hidden p-2 bg-danger-subtle'>
+      <main className='flex-fill'>
         {showPage == 'home' && <Home setShowPage={setShowPage} />}
         {showPage == 'game' && <Game setShowPage={setShowPage} />}
       </main>
 
-      <footer className='text-center p-2 m-2'>
+      <footer className='text-center p-2 m-0'>
           Designed and created by YcWong @2024
       </footer>
 
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
-import home01Pic from '/home01.png?url'
+import game01Img from '/game01.jpg?url'
 
 function Game({ setShowPage }) {
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center'>
-      aaa
+    <div className='d-flex flex-column'>
+      {/* fixed top bar */}
+      <div className='fixed-top d-flex bg-secondary'>
+        hi
+      </div>
+
+      {/* game image */}
+      <div className='mt-5'>
+        <img src={game01Img} alt="" className='w-100'/>
+      </div>
+
     </div>
   )
 }
